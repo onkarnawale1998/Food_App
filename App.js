b4e4a0c9e44590da4c347b24fb004bd07aeda67a
onkarnawale1998 <onkarnawale1998@gmail.com>
2024-05-04 23:24:51 +0530

    nested  tages with React.createElement

--- App.js
+++ App.js
@@ -10,4 +10,19 @@ const heading1 = React.createElement(
   "Hello World from React"
 ); // heading1 is a react element/object not actual h1 element
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(heading1);
+
+// if we haved div with multople children then do as follow
+const parent = React.createElement("div", { id: "parent" }, [
+    "Hello World from parent",
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "Hello World from child 1"),
+    React.createElement("h2", {}, "Hello World from child 1"),
+    React.createElement("h3", {}, "Hello World from child 1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hello World from child 2"),
+    React.createElement("h2", {}, "Hello World from child 2"),
+    React.createElement("h3", {}, "Hello World from child 2"),
+  ]),
+]);
+root.render([heading1, parent]);
