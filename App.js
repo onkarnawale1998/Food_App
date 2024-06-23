561fb5abe40708df51f506320a63b93f4cedb53c
onkarnawale1998 <onkarnawale1998@gmail.com>
2024-06-23 16:05:48 +0530

    syntax

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const App = () => <h1>Hello this basic react template made with parcel ❤️🚀 </h1>;
+const App = () => (
+  <h1>Hello this basic react template made with parcel ❤️🚀 </h1>
+);
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
