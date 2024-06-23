a4098bf368077d38beec89c9a1721d8c873c998b
onkarnawale1998 <onkarnawale1998@gmail.com>
2024-06-23 11:39:30 +0530

    Basic React template

--- App.js
+++ App.js
@@ -1,30 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-// JavaScript Code
-const heading = document.createElement("h1");
-heading.textContent = "Hello World from JavaScript";
-document.querySelector(".hello").appendChild(heading);
+import { createRoot } from "react-dom/client";
 
-// React Code
-const heading1 = React.createElement(
-  "h1",
-  { id: "title" },
-  "Hello World from React"
-); // heading1 is a react element/object not actual h1 element
-const root = ReactDOM.createRoot(document.querySelector("#root"));
+const App = () => <h1>Hello this basic react template made with parcel ❤️🚀 </h1>;
 
-// if we haved div with multople children then do as follow
-const parent = React.createElement("div", { id: "parent" }, [
-    "Hello World from parent",
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Hello World from child 1"),
-    React.createElement("h2", {}, "Hello World from child 1"),
-    React.createElement("h3", {}, "Hello World from child 1"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hello World from child 2"),
-    React.createElement("h2", {}, "Hello World from child 2"),
-    React.createElement("h3", {}, "Hello World from child 2"),
-  ]),
-]);
-root.render([heading1, parent]);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
