2f6ada64e1eff85f619c7e751a4bf2e99da99c37
onkarnawale1998 <onkarnawale1998@gmail.com>
2024-11-30 14:13:50 +0530

    toggle the filtered restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,13 +4,23 @@ import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
     const [resListState, setResListState] = useState(resList);
+    const [isFiltered, setIsFiltered] = useState(false);
+
+    const handleFilter = () => {
+        if (isFiltered) {
+            setResListState(resList);
+            setIsFiltered(false);
+        } else {
+            const filteredResList = resList.filter((res) => res.info.avgRatingString > 4);
+            setResListState(filteredResList);
+            setIsFiltered(true);
+        }
+    };
+
     return (
         <div className="body">
             <div className="filter">
-                <button className="filter-btn" onClick={() => {
-                    const filteredResList = resList.filter((res) => res.info.avgRatingString > 4);
-                    setResListState(filteredResList);
-                }}>Top Rated Restaurants</button>
+                <button className="filter-btn" onClick={handleFilter}>Top Rated Restaurants</button>
             </div>
             <div className="res-container">
                 {resListState.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)}
