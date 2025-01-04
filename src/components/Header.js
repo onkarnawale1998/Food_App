9c2b19156cc4acdae29f71a12c841aaf7a85109c
onkarnawale1998 <onkarnawale1998@gmail.com>
2025-01-04 21:20:00 +0530

    Login and logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login")
   return (
     <div className="header" >
       <div className="logo-container">
@@ -12,6 +14,9 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button className="login-btn" onClick={() => {
+            btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+          }}>{btnName}</button>
         </ul>
       </div>
     </div >
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
 
-export const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.4088&lng=76.5604&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const LOGO_URL = "https://thumbs.dreamstime.com/b/food-delivery-logo-design-template-134749604.jpg"
