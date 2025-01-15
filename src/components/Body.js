4db3d3bc10fe9f5b8db6302ac90230f93c0913d6
onkarnawale1998 <onkarnawale1998@gmail.com>
2025-01-15 23:22:58 +0530

    Added Search funtionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,6 +8,7 @@ const Body = () => {
     const [originalList, setOriginalList] = useState([]);
     const [isFiltered, setIsFiltered] = useState(false);
     const [buttonName, SetButtonName] = useState("Top Rated Restaurants");
+    const [searchTxt, setSearchTxt] = useState("");
 
     useEffect(() => {
         fetchData();
@@ -16,7 +17,7 @@ const Body = () => {
     const fetchData = async () => {
         const data = await fetch(SWIGGY_API)
         const json = await data.json();
-        const fetchedData = json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
+        const fetchedData = json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
         setOriginalList(fetchedData);
         setResListState(fetchedData);
     }
@@ -27,7 +28,7 @@ const Body = () => {
             setIsFiltered(false);
             SetButtonName("Top Rated Restaurants");
         } else {
-            const filteredResList = originalList.filter((res) => res.info.avgRatingString >= 4);
+            const filteredResList = resListState.filter((res) => res.info.avgRating >= 4.1);
             setResListState(filteredResList);
             setIsFiltered(true);
             SetButtonName("All Restaurants");
@@ -36,6 +37,13 @@ const Body = () => {
     return resListState.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" onChange={(e) => setSearchTxt(e.target.value)} value={searchTxt} />
+                    <button className="search-btn" onClick={() => {
+                        const filteredRestaurants = resListState.filter((res) => res?.info?.name.toLowerCase()?.includes(searchTxt.toLowerCase()));
+                        setResListState(filteredRestaurants);
+                    }}>Search</button>
+                </div>
                 <button className="filter-btn" onClick={handleFilter}>{buttonName}</button>
             </div>
             <div className="res-container">
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -3,6 +3,8 @@ import React from "react";
 const Shimmer = () => {
     return (
         <>
+            <input type="text" disabled />
+            <button className="search-btn" disabled >Search</button>  
             <button className="filter-btn-shimmer" disabled >Top Rated Restaurants</button>
             <div className="shimmer-container">
                 <div className="shimmer-card"></div>
